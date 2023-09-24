12c55bf4dbd2a24a7380a35085ed153c0722eecd
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2023-09-25 03:45:19 +0530

    Created: testbench code for full subtractor

---
+++ FullSubtractor/full_subtractor.v
@@ -0,0 +1,54 @@
+`timescale 1ns/1ps
+`include "full_subtractor.v"
+
+module full_subtractor_tb();
+
+    reg a, b, bin;
+    wire D, bout;
+
+    full_subtractor full_subtractor_uut(a, b, bin, D, bout);
+
+    initial begin
+        $dumpfile("full_subtractor_tb.vcd");
+        $dumpvars(0, full_subtractor_tb);
+
+        a = 0;
+        b = 0;
+        bin = 0;
+        #20;
+
+        a = 0;
+        b = 0;
+        bin = 1;
+        #20;
+
+        a = 0;
+        b = 1;
+        bin = 1;
+        #20;
+
+        a = 1;
+        b = 0;
+        bin = 0;
+        #20;
+
+        a = 1;
+        b = 0;
+        bin = 1;
+        #20;
+
+        a = 1;
+        b = 1;
+        bin = 0;
+        #20;
+
+        a = 1;
+        b = 1;
+        bin = 1;
+        #20;
+
+        $display("Test complete");
+        $finish;
+    end
+
+endmodule
