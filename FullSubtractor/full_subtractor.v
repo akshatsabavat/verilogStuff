601792555cc4ba2d2443efce6dd8850722353de6
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2023-09-25 03:58:03 +0530

    Fixed: errors in assignment in the module

--- FullSubtractor/full_subtractor.v
+++ FullSubtractor/full_subtractor.v
@@ -1,9 +1,9 @@
-module full_subtractor (a, b, bin, D, bout);
-
-    input a, b, bin;
-    output D, bout;
+module full_subtractor (
+    input a, b, bin,
+    output D, bout
+);
 
     assign D = a ^ b ^ bin;
-    assign bout = = (~a & b) | ((~a | b) & bin);
+    assign bout = (~a & b) | ((~a ^ b) & bin);
 
 endmodule
