1e2b568bf53ed6cde51856bcb867a7645d81269d
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2023-09-25 04:20:06 +0530

    Created: Module defination of on bit comparator

---
+++ oneBitComparator/one_bit_comparator.v
@@ -0,0 +1,10 @@
+module one_bit_comparator (
+    input a, b;
+    output lesser_comparator, equals_comparator, greater_comparator 
+);
+
+assign lesser_comparator = (~a) & b;
+assign greater_comparator = a & (~b);
+assign equals_comparator = a ~^ b
+
+endmodule
